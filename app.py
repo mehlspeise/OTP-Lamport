ab75e989ccbdd1e6bd36736e37ff9eff4bc6e6f0
Paras <paras.sood@hotmail.com>
2020-09-24 18:51:33 +0200

    userRegistration

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import hashlib
+from flask_bcrypt import bcrypt, generate_password_hash
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,7 +11,8 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    user = db.Column(db.String(200), nullable=False)
+    pwd = db.Column(db.String(200), nullable = False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
@@ -18,8 +22,10 @@ class Todo(db.Model):
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+        task_user = request.form.get('user', False)
+        task_pwd = request.form.get('pwd', False)
+        pwd_hash = generate_password_hash(task_pwd, 12)
+        new_task = Todo(user=task_user, pwd= pwd_hash)
 
         try:
             db.session.add(new_task)
@@ -42,23 +48,9 @@ def delete(id):
         db.session.commit()
         return redirect('/')
     except:
-        return 'There was a problem deleting that task'
+        return 'There was a problem deleting that user'
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
+#update function
 
 
 if __name__ == "__main__":
